c9dbbe04c276f0821cd3772e82aaef1c0829951b
Alexander Kurz <alex.kurz95@gmail.com>
2025-04-23 15:14:25 +0200

    Update datamodule

--- train.py
+++ train.py
@@ -1,12 +1,36 @@
 # Example from https://lightning.ai/lightning-ai/studios/train-a-diffusion-model-with-pytorch-lightning?section=featured
 from jsonargparse import CLI
 from lightning import Trainer
+from torch.utils.data import DataLoader, Dataset
 
-from src.datamodule import DiffusionData
+from src.datamodule import DiffusionData, DreamBoothDataset, collate_fn
 from src.pl_module import DiffusionModel
 
 
-def main():
+def main(args):
+    # Dataset and DataLoaders creation:
+    train_dataset = DreamBoothDataset(
+        instance_data_root=args.instance_data_dir,
+        instance_prompt=args.instance_prompt,
+        class_data_root=args.class_data_dir if args.with_prior_preservation else None,
+        class_prompt=args.class_prompt,
+        class_num=args.num_class_images,
+        tokenizer=tokenizer,
+        size=args.resolution,
+        center_crop=args.center_crop,
+        encoder_hidden_states=pre_computed_encoder_hidden_states,
+        class_prompt_encoder_hidden_states=pre_computed_class_prompt_encoder_hidden_states,
+        tokenizer_max_length=args.tokenizer_max_length,
+    )
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=args.train_batch_size,
+        shuffle=True,
+        collate_fn=lambda examples: collate_fn(examples, args.with_prior_preservation),
+        num_workers=args.dataloader_num_workers,
+    )
+
     model = DiffusionModel()
     data = DiffusionData()
     trainer = Trainer(max_epochs=2, precision="bf16-mixed")
