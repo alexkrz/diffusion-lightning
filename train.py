77a1a06c7fff3166d177d258ce7c681b2f8623be
Alexander Kurz <alex.kurz95@gmail.com>
2025-04-24 16:19:01 +0200

    Use dataclass for collecting command-line arguments

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
+from dataclasses import dataclass
 from typing import Optional
 
-from jsonargparse import CLI, Namespace
+from jsonargparse import CLI
 from lightning import Trainer
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoTokenizer
@@ -9,47 +10,39 @@ from src.datamodule import DiffusionData, DreamBoothDataset, collate_fn
 from src.pl_module import DiffusionModel
 
 
-def parse_args(
-    pretrained_model_name_or_path: str = "stable-diffusion-v1-5/stable-diffusion-v1-5",
-    instance_data_dir: str = "./data/dog",
-    instance_promt: str = "a photo of sks dog",
-    resolution: int = 512,
-    revision: Optional[str] = None,
-    pre_computed_text_embebeddings: Optional[str] = None,
-    class_prompt: Optional[str] = None,
-):
-    """Script to train a diffusion model with Dreambooth and LoRA."""
-    args = Namespace(locals())
-    return args
+@dataclass
+class Args:
+    """CLI for training a diffusion model using Dreambooth and LoRA."""
+
+    pretrained_model_name_or_path: str = "stable-diffusion-v1-5/stable-diffusion-v1-5"
+    instance_data_dir: str = "./data/dog"
+    instance_prompt: str = "a photo of sks dog"
+    resolution: int = 512
+    with_prior_preservation: bool = False
+    train_batch_size: int = 4
+    dataloader_num_workers: int = 0
 
 
-def main(args):
-    # Make some assertions
-    assert args.pre_computed_text_embebeddings is None
-    pre_computed_encoder_hidden_states = None
-    assert args.class_prompt is None
-    pre_computed_class_prompt_encoder_hidden_states = None
-
+def main(args: Args):
     # Dataset and DataLoaders creation:
     tokenizer = AutoTokenizer.from_pretrained(
         args.pretrained_model_name_or_path,
         subfolder="tokenizer",
-        revision=args.revision,
         use_fast=False,
     )
 
     train_dataset = DreamBoothDataset(
         instance_data_root=args.instance_data_dir,
         instance_prompt=args.instance_prompt,
-        class_data_root=args.class_data_dir if args.with_prior_preservation else None,
-        class_prompt=args.class_prompt,
-        class_num=args.num_class_images,
         tokenizer=tokenizer,
+        class_data_root=None,
+        class_prompt=None,
+        class_num=None,
         size=args.resolution,
-        center_crop=args.center_crop,
-        encoder_hidden_states=pre_computed_encoder_hidden_states,
-        class_prompt_encoder_hidden_states=pre_computed_class_prompt_encoder_hidden_states,
-        tokenizer_max_length=args.tokenizer_max_length,
+        center_crop=False,
+        encoder_hidden_states=None,
+        class_prompt_encoder_hidden_states=None,
+        tokenizer_max_length=None,
     )
 
     train_dataloader = DataLoader(
@@ -67,5 +60,5 @@ def main(args):
 
 
 if __name__ == "__main__":
-    args = CLI(parse_args, as_positional=False)
+    args = CLI(Args, as_positional=False)
     main(args)
