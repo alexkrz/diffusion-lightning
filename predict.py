f1c0b9db91e1e002b8af88f48a220cc7f7a1e468
Alexander Kurz <alex.kurz95@gmail.com>
2025-04-28 15:37:19 +0200

    Try different hyperparameters

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ from jsonargparse import CLI
 
 
 def main(
-    lora_weights_fp: str = "lightning_logs/version_0/lora_weights.safetensors",
+    lora_weights_fp: str = "lightning_logs/version_4/lora_weights.safetensors",
     prompt: str = "A photo of sks dog in a bucket",
     save_fp: str = "results/dog-bucket.png",
 ):
@@ -15,7 +15,7 @@ def main(
 
     pipeline.load_lora_weights(lora_weights_fp)
 
-    image = pipeline(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+    image = pipeline(prompt, num_inference_steps=100, guidance_scale=7.5).images[0]
     image.save(save_fp)
 
 
